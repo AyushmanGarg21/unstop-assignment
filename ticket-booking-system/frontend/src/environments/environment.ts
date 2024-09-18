22af966ccc4e03b0f9563cdf74dce0edf04a4727
Ayushmangarg21 <asmgarg74@gmail.com>
2024-09-18 22:42:53 +0530

    deployment url change

--- ticket-booking-system/frontend/src/environments/environment.ts
+++ ticket-booking-system/frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     // apiUrl: 'http://localhost:3000/api',
-    apiUrl: 'https://booking-web-service.onrender.com/api',
+    // apiUrl: 'https://booking-web-service.onrender.com/api',
+    apiUrl: 'https://unstop-assignment-production.up.railway.app/api',
   };
